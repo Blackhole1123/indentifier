0b635dc730b0538806392ef6d43165de98222151
Rishaan Desai <77017806+Blackhole1123@users.noreply.github.com>
2022-08-17 16:05:42 -0700

    fixed #1

--- test.js
+++ test.js
@@ -1,27 +1,27 @@
 const file = process.argv[2];
 
 function fix(file) {
-    const fs = require('fs');
-    const data = fs.readFileSync(file, 'utf-8').split('\n').map(line => {
-        for (let i = 0; i < line.length; i++) {
-            if (line[i] !== ' ') {
-                line = line.slice(i);
-                break;
-            }
-            for (let i = line.length - 1; i >= 0; i--) {
-                if (line[i] !== ' ') {
-                    line = line.slice(0, i + 1);
-                    break;
-                }
-            }
-        }
-        return line;
-    }).join('\n');
+const fs = require('fs');
+const data = fs.readFileSync(file, 'utf-8').split('\n').map(line => {
+for (let i = 0; i < line.length; i++) {
+                                    if (line[i] !== ' ') {
+line = line.slice(i);
+break;
+}
+for (let i = line.length - 1; i >= 0; i--) {
+if (line[i] !== ' ') {
+line = line.slice(0, i + 1);
+break;
+}
+}
+}
+return line;
+}).join('\n');
 
-    fs.writeFileSync(file, data);
+fs.writeFileSync(file, data);
 }
 
 if (require.main === module) {
-    fix(file);
+fix(file);
 }
 module.exports = fix;
